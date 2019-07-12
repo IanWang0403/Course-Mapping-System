e6d584a47b63c4c96adf936e9743bde454adc091
Yining Wang <yining.wang@student.adelaide.edu.au>
2019-07-12 17:34:49 +0930

    Deleted comments
    Deleted comments and ready to creat new one

--- src/javascripts/controller.js
+++ src/javascripts/controller.js
@@ -1,31 +1,18 @@
-// Create the input graph
-
-
 var g = new dagreD3.graphlib.Graph()
   .setGraph({})
   .setDefaultEdgeLabel(function() { return {}; });
 
-// Set up an SVG group so that we can translate the final graph.
 var svg = d3.select("svg"),
   inner = svg.append("g");
 
-// Here we"re setting nodeclass, which is used by our custom drawNodes function
-// below.
 d3.csv("database_test.csv").then(function(data) {
 
-  
-  //console.log(data[1])
   for (var i = 0; i < data.length; i++) {
-    //if(data[i].course_pr != "None"){
     g.setNode(data[i].course_id, {label: data[i].course_name, description: data[i].course_detail})
-    //inner.attr("name", data[i].course_id);
-    //}
   }
 
   for (var i = 0; i < data.length; i++) {
-    //if(data[i].course_pr != "None"){
       if(data[i].course_pr.search(/, | or |; | and /) == -1){
-        //console.log(data[i].course_pr);
         if(g.hasNode(data[i].course_pr)){
           g.setEdge(data[i].course_pr, data[i].course_id, {
             curve: d3.curveBasis 
@@ -35,31 +22,25 @@ d3.csv("database_test.csv").then(function(data) {
       }
       else{
         tmpData = data[i].course_pr.split(/, | or |; | and /);
-        //console.log(tmpData);
         
         for(var j = 0; j < tmpData.length; j++){
           
           if(g.hasNode(tmpData[j])){
-            //console.log(tmpData[j] + "," + data[i].course_id);
             if(data[i].course_pr.search("and") != -1){
               g.setEdge( tmpData[j], data[i].course_id,{
                 curve: d3.curveBasis,
-                //style: "stroke-dasharray: 5, 5;",
                 arrowheadStyle: "fill :#f66"
               });
             }else{
               g.setEdge( tmpData[j], data[i].course_id, {
                 curve: d3.curveBasis,
-                //style: "stroke: #333",
               });
             }
             
 
           }
         }
-      }
-
-    //}     
+      }   
   }
 
   g.graph().rankDir = 'LR';
@@ -80,17 +61,12 @@ d3.csv("database_test.csv").then(function(data) {
 
   var render = new dagreD3.render();
 
-  
-
-  // Run the renderer. This is what draws the final graph.
   render(inner, g);
 
   inner.selectAll("g.node")
     .attr("code", function(v){return v})
 
-
   inner.selectAll("g.node")
-    //.on(on("mouseclick",function(v){console.log(g.node(v).description)}))
     .attr("title", function(v) { return styleTooltip(g.node(v).label, v, g.node(v).description) })
     .each(function(v) { $(this).tipsy( { gravity: "n", opacity: 0.8, html: true }); });
     
